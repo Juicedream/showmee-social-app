1f5d7678747d1dbafd6b0cb64cbac1cbd3d93cea
Juicedream <judexfrayo@gmail.com>
2024-12-31 17:47:38 +0100

    Implemented Clerk authentication and added a sign in button from shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello from home page</h1>
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <Button variant={"secondary"}>Click Me</Button>
     </div>
   );
 }
