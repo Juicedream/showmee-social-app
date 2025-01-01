7f6899c29611ba9e2bd366d78a16fcb9d399f43f
Juicedream <judexfrayo@gmail.com>
2025-01-01 14:37:54 +0100

    Integrated the database with prisma and connected with clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,33 @@
+import { helperPrompt } from "@/helperPrompt";
+import Link from "next/link";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-import { helperPrompt } from "@/helperPrompt"
-import Link from "next/link"
-import DesktopNavbar from "./DesktopNavbar"
-import MobileNavbar from "./MobileNavbar"
-
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if(user) await syncUser()
   return (
-   <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
-    <div className="max-w-7xl mx-auto px-4">
-      <div className="flex items-center justify-between h-16">
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
           <div className="flex">
             <Link
-            href="/"
-            className="text-xl font-bld text-primary font-mono tracking-wider"
+              href="/"
+              className="text-xl font-bld text-primary font-mono tracking-wider"
             >
               {/* Showmee title from helper prompt.ts */}
-            {helperPrompt.title}
+              {helperPrompt.title}
             </Link>
           </div>
 
           <DesktopNavbar />
           <MobileNavbar />
+        </div>
       </div>
-    </div>
-   </nav>
-  )
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
